7264fde9a7e8e41aacc4eb3cb9fc66aa6d1d5f5e
IbrahimGustav <crthrsmrx@gmail.com>
2024-02-29 15:14:11 +0700

    added project to reverse name

--- session3/variables.py
+++ session3/variables.py
@@ -2,6 +2,22 @@ name = "Ibrahim Gustav Amany"
 age = 20
 ismale = True
 
+# assign one value to multiple variable
+a = b = c = 1
+c = 2
+d = "2"
+
+print("a is:", a)
+print("b is:", b)
+print("c is:", c)
+print("d is:", d)
+print(b + c)
+print (a + b)
+
+# check type of variable
+print("C data type is:", type(c))
+print("D data type is:", type(d))
+
 print("Hello my name is", name)
 print("I am", age)
 print("I am male: ", ismale)
