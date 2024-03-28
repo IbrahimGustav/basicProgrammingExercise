eda8593769b0d514ae68014b6042b0341f811e27
IbrahimGustav <crthrsmrx@gmail.com>
2024-03-28 14:39:54 +0700

    fixed age group

--- session6/age_group.py
+++ session6/age_group.py
@@ -1,8 +1,10 @@
 age = int(input("Please input your age: "))
 
-if age <= 12:
+if age <= 12 and age >= 0:
     print ("You are a child")
 elif age <= 19:
     print ("You are a teenager")
-else:
+elif age <= 150:
     print ("You are an adult")
+else:
+    print ("Please input a valid age")
